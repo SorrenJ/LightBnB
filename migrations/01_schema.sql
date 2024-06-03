799b6746da2c3519d5aee347a8a0a1b0eac03515
SorrenJ <soalexjao@gmail.com>
2024-06-02 19:51:56 -0700

    schema tables almost complete (no foreign keys yet)

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -5,9 +5,41 @@ CREATE TABLE users (
   password VARCHAR(255),
 );
 
-CREATE TABLE properties (
+
+CREATE TABLE reservation (
   id INTEGER PRIMARY KEY,
-  name VARCHAR(255),
-  email VARCHAR(255),
-  password VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  property_id INTEGER,
+  guest_id INTEGER
+);
+
+
+CREATE TABLE property_reviews (
+  guest_id INTEGER,
+  property_id INTEGER,
+ reservation_id INTEGER,
+ rating SMALLINT,
+ message TEXT
+);
+
+
+
+CREATE TABLE properties (
+    id INT PRIMARY KEY,
+    owner_id INTEGER,
+    title VARCHAR(255),
+    description TEXT,
+    thumbnail_photo_url VARCHAR(255),
+    cover_photo_url VARCHAR(255),
+    cost_per_night INTEGER,
+    parking_spaces INTEGER,
+    number_of_bathrooms INTEGER,
+    number_of_bedrooms INTEGER,
+    country VARCHAR(255),
+    street VARCHAR(255),
+    city VARCHAR(255),
+    province VARCHAR(255),
+    post_code VARCHAR(255),
+    active BOOLEAN
 );
