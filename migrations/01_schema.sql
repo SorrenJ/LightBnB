d2b894a587e8f58251d56880ecc6e061db159db0
SorrenJ <soalexjao@gmail.com>
2024-06-02 19:17:16 -0700

    inital schema added (incomplete)

---
+++ migrations/01_schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
+);
+
+CREATE TABLE properties (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255),
+);
