8ada1bf6062ef3bc452416611ab4bef97ca9d2db
SorrenJ <soalexjao@gmail.com>
2024-06-06 15:57:57 -0700

    querying the database and verifying connection

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -2,6 +2,23 @@ const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
 /// Users
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+// the following assumes that you named your connection variable `pool`
+pool.query(`SELECT title 
+FROM properties 
+LIMIT 10;`
+
+).then(response => {
+  console.log(response)
+})
 
 /**
  * Get a single user from the database given their email.
