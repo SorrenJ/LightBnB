94a8d3895b57cac16c4fed2bd4ef56ac4f06e199
SorrenJ <soalexjao@gmail.com>
2024-06-10 15:23:01 -0700

    My Reservation refactored to see user's reservations

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -116,9 +116,42 @@ const addUser = (user) => {
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+
+const getAllReservations = (guest_id) => {
+ 
+  const query = `
+    SELECT r.id, p.title, p.cost_per_night, r.start_date, AVG(pr.rating) as average_rating
+    FROM reservations r
+    JOIN properties p ON r.property_id = p.id
+    JOIN property_reviews pr ON p.id = pr.property_id
+    WHERE r.guest_id = $1
+    GROUP BY p.id, r.id
+    ORDER BY r.start_date
+    LIMIT 10;
+  `;
+  
+  // Use an array to pass the userID securely as a parameter to the query
+  const values = [guest_id];
+
+  
+  return pool.query(query, values)
+    .then((result) => {
+   
+      return result.rows;
+    })
+    .catch((error) => {
+     
+      console.error('Error executing getAllReservations:', error);
+      return null;
+    });
 };
+
+
+
+// const getAllReservations = function (guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
 
 /// Properties
 
@@ -147,6 +180,10 @@ const getAllProperties = function (options, limit = 10) {
   // }
   // return Promise.resolve(limitedProperties);
 };
+
+
+
+
 
 /**
  * Add a property to the database
