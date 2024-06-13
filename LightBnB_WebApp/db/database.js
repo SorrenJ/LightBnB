629d72e5b4a07b4febf04d8aec77d7a505f5c943
SorrenJ <soalexjao@gmail.com>
2024-06-12 18:58:14 -0700

    getAllProperties : added WHERE 1=1 and placed GROUP BY before HAVING

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -176,12 +176,13 @@ const getAllProperties = function (options, limit = 10) {
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id
+  WHERE 1=1
   `;
 
   // 3
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;
+    queryString += `city LIKE $${queryParams.length} `;
   }
 
 
@@ -201,6 +202,15 @@ const getAllProperties = function (options, limit = 10) {
     queryString += ` AND cost_per_night <= $${queryParams.length} `;
   }
   
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+
   if (options.minimum_rating) {
     queryParams.push(options.minimum_rating);
     queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
@@ -209,13 +219,7 @@ const getAllProperties = function (options, limit = 10) {
 
 
 
-  // 4
-  queryParams.push(limit);
-  queryString += `
-  GROUP BY properties.id
-  ORDER BY cost_per_night
-  LIMIT $${queryParams.length};
-  `;
+
 
   // 5
   console.log(queryString, queryParams);
